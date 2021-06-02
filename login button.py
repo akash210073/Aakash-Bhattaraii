ef2ec3ac4b9ee691ab78cfc376b59c9997f9618a
aakash210073 <aakashbhatry97@gmail.com>
2021-06-02 10:44:31 +0545

    aa

---
+++ login button.py
@@ -0,0 +1,168 @@
+import tkinter
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+import os
+
+root = Tk()
+root.title('Login Page')
+root.geometry("700x500")
+root.resizable(0,0)
+root.configure(bg="black")
+
+
+
+
+# Creating User Entry
+username_label = Label(root, bg="black", fg="white", text="Username: ", font=("Times New Roman", 20, "bold")).place(x=80, y=103)
+
+username = Entry(root, width=30, borderwidth=10, fg="black", bg="#84BA48", font=("Times New Roman", 20, "bold"), justify=LEFT)
+username.place(x=220, y=100)
+
+
+
+# Creating Password Entry
+password_label = Label(root, bg="black", fg="white", text="Password: ", font=("Times New Roman", 20, "bold")).place(x=80, y=183)
+
+password = Entry(root, width=30, borderwidth=10, fg="white", bg="#84BA48", font=("Times New Roman", 20, "bold"), justify=LEFT)
+password.config(show="*")
+password.place(x=220, y=180)
+
+
+
+def login_successful():
+    messagebox.showinfo("ADMIN","You have logged in successfully!")
+    root.destroy()
+    os.system("main.py")
+
+
+def try_login():
+    conn = sqlite3.connect('registration.db')
+
+    c = conn.cursor()
+
+    c.execute("SELECT *, oid FROM reg_data")
+    records = c.fetchall()
+    # Getting user login details from the login page
+    name = username.get()
+    pwd = password.get()
+    # Loop through the results
+    print_record = ''
+    for record in records:
+        print_record += str(record[0]) + ' ' + str(record[1]) + ' ' + str(record[3]) + "\n"
+
+        if record[0] != name or record[2] != pwd or record[3] != pwd:
+            # login_unsuccessful
+            continue
+        elif record[0] == name and record[2] == pwd and record[3] == pwd:
+            login_successful()
+    conn.commit()
+    conn.close()
+
+def new_registration():
+    root.destroy()
+    reg_page = Tk()
+    reg_page.title('Registration Page')
+    reg_page.geometry("650x500")
+    reg_page.resizable(0, 0)
+    reg_page.configure(bg="black")
+    # # Creating a database or connect to one
+    conn = sqlite3.connect('registration.db')
+
+    # Create Cursor
+    c = conn.cursor()
+
+
+    # # Create Table for storing registration details
+    #
+    # c.execute(""" CREATE TABLE reg_data(
+    #         user_name text,
+    #         user_address text,
+    #         user_pass text,
+    #         user_confirm_pass text
+    # )
+    # """)
+    # messagebox.showinfo("User Registration Table Created Successfully", "You can now add the data")
+
+    # Creating text boxes
+    new_reg_label = Label(reg_page, text="New Registration", font=("Times New Roman", 23, "bold"),fg='white', bg='black', bd=4, relief=RAISED).place(x=150, y=30)
+
+    user_name_label = Label(reg_page, text="User Name:", font=("Times New Roman", 23, "bold"),bg="black",fg="white").place(x=30, y=110)
+
+    user_name = Entry(reg_page, font=("Times New Roman", 23, "bold"))
+    user_name.place(x=300, y=110)
+
+    address_label = Label(reg_page, text="Address:", font=("Times New Roman", 21, "bold"),bg="black",fg="white").place(x=30, y=170)
+
+    address = Entry(reg_page, font=("Times New Roman", 23, "bold"))
+    address.place(x=300, y=170)
+
+    password_label = Label(reg_page, text="Password:", font=("Times New Roman", 21, "bold"),bg="black",fg="white").place(x=30, y=230)
+
+    password = Entry(reg_page, font=("Times New Roman", 23, "bold"))
+    password.place(x=300, y=230)
+
+    confirm_password_label = Label(reg_page, text="Confirm Password:", font=("Times New Roman", 21, "bold"),bg="black",fg="white").place(x=30,
+                                                                                                                  y=290)
+
+    confirm_password = Entry(reg_page, font=("Times New Roman", 23, "bold"))
+    confirm_password.place(x=300, y=290)
+
+    # Creating a  submit button for the database
+
+    def register():
+        # Create a database or connect to one
+        conn = sqlite3.connect('registration.db')
+        c = conn.cursor()
+
+        # Insert into table
+        c.execute("INSERT INTO reg_data VALUES (:user_name, :address, :password, :confirm_password)", {
+            'user_name': user_name.get(),
+            'address': address.get(),
+            'password': password.get(),
+            'confirm_password': confirm_password.get()
+        })
+
+        conn.commit()
+        conn.close()
+
+        user_name.delete(0, END)
+        address.delete(0, END)
+        password.delete(0, END)
+        confirm_password.delete(0, END)
+
+        messagebox.showinfo("User Data", "YOUR DATA IS REGISTERED")
+        reg_page.destroy()
+        os.system("python main.py")
+
+    # Creating a function for showing the records to check if the data is being stored
+
+    def view_records():
+        # Creating a database or connect to one
+        conn = sqlite3.connect('registration.db')
+        # Create Cursor
+        c = conn.cursor()
+        # Query of the databse
+        c.execute("SELECT *, oid FROM reg_data")
+        records = c.fetchall()
+        print(records)
+        conn.commit()
+        conn.close()
+
+    # Creating a submit button
+    register_btn = Button(reg_page, text="Register", font=("Times New Roman", 21, "normal"), command=register)
+    register_btn.place(x=330, y=350)
+
+    # View Record Button to test if the data is being stored
+    show_record_btn = Button(text="Show Records", font=('Century Gothic', 20, 'normal'), command=view_records)
+    show_record_btn.place(x=60, y=350)
+    reg_page.mainloop()
+
+# Creating Login Button
+login_btn = Button(root, width=20, borderwidth=10, bg="white", text="Login", font=("Times New Roman", 22, "bold"),command=try_login )\
+    .place(x=250, y=250)
+
+new_user_reg_label = Button(root, width=20, borderwidth=10, bg="white", text="For New User Click Here!", font=("Times New Roman", 22, "bold"), command=new_registration)\
+    .place(x=250, y=350)
+
+root.mainloop()
--- main.py
+++ main.py
@@ -1,93 +1,325 @@
+import os
 from tkinter import*
-root=Tk()
-root.title('Subscriber Management System')
-root.geometry('1800x800')
-myLabel = Label(root, text='SUBSCRIBER MANAGEMENT SYSTEM', font=('times new roman',25, 'bold'), fg='red', bg='black', bd=4, relief=RAISED)
+from tkinter import ttk
+import tkinter as tk
+from tkinter import messagebox
+import sqlite3
+# from tabulate import tabulate
+import sys
+
+root=tk.Tk()
+root.title('SUBSCRIBER MANAGEMENT SYSTEM')
+root.geometry('1700x700')
+
+# Creating Table
+# Creating a database or connect to one
+conn = sqlite3.connect('database.db')
+
+# Create Cursor
+c = conn.cursor()
+
+# Create Table for storing registration details
+# c.execute(""" CREATE TABLE sub_data(
+#         subscriber_userid text,
+#         subscriber_name text,
+#         subscriber_email text,
+#         subscriber_contact text,
+#         subscriber_package text,
+#         subscriber_address text
+# )
+# """)
+# messagebox.showinfo("User Registration Table Created Successfully", "You can now add the data")
+
+myLabel = Label(root, text='SUBSCRIBER MANAGEMENT SYSTEM', font=('times new roman',30, 'bold'), fg='white', bg='black', bd=4, relief=RAISED)
 myLabel.pack(side=TOP, fill=X)
+
 #manage frame
-my_frame = Frame(root, bd=4, relief = RIDGE, bg= 'BLACK')
-my_frame.place(x=10, y= 80, width=600, height=650)
+my_frame = Frame(root, bd=4, relief = RIDGE, bg= 'black')
+my_frame.place(x=20, y= 80, width=550, height=600)
+
 #manage title
-M_title=Label(my_frame,text='MANAGE SUBSCRIBER', font=('Arial Black',20, 'bold'), fg='white', bg='black', bd=4, relief=RAISED)
+M_title=Label(my_frame,text='MANAGE SUBSCRIBER', font=('times new roman',30, 'bold'), fg='white', bg='black', bd=4, relief=RAISED)
 M_title.grid(row=0, columnspan=2, padx=20, pady=20)
+
 #labels
-lbl_id=Label(my_frame, text='CUSTOMER ID:', font=('Arial Black',16,'bold'),fg='white',bg='black')
-lbl_id.grid(row=1, column=0, padx=20, pady=10, sticky=W)
-id_entry=Entry(my_frame,font=('Arial Black',16,'bold'))
-id_entry.grid(row=1, column=1, padx=20, pady=10, sticky=W)
-lbl_name=Label(my_frame, text='NAME:', font=('Arial Black',16,'bold'),fg='white',bg='black')
+lbl_userid=Label(my_frame, text='USER ID', font=('times new roman',20,'bold'),fg='white',bg='black')
+lbl_userid.grid(row=1, column=0, padx=20, pady=10, sticky=W)
+
+roll_entry=Entry(my_frame,font=('times new roman',20,'bold'))
+roll_entry.grid(row=1, column=1, padx=20, pady=10, sticky=W)
+
+lbl_name=Label(my_frame, text='NAME', font=('times new roman',20,'bold'),fg='white',bg='black')
 lbl_name.grid(row=2, column=0, padx=20, pady=10, sticky=W)
-name_entry=Entry(my_frame,font=('Arial Black',16,'bold'))
+
+name_entry=Entry(my_frame,font=('times new roman',20,'bold'))
 name_entry.grid(row=2, column=1, padx=20, pady=10, sticky=W)
-lbl_package=Label(my_frame, text='PACKAGE:', font=('Arial Black',16,'bold'),fg='white',bg='black')
-lbl_package.grid(row=4, column=0, padx=20, pady=10, sticky=W)
+
+lbl_email=Label(my_frame, text='EMAIL', font=('times new roman',20,'bold'),fg='white',bg='black')
+lbl_email.grid(row=3, column=0, padx=20, pady=10, sticky=W)
+
+email_entry=Entry(my_frame,font=('times new roman',20,'bold'))
+email_entry.grid(row=3, column=1, padx=20, pady=10, sticky=W)
+
+lbl_gender=Label(my_frame, text='GENDER', font=('times new roman',20,'bold'),fg='white',bg='black')
+lbl_gender.grid(row=4, column=0, padx=20, pady=10, sticky=W)
+
 def show():
     myLabel= Label(root, text=clicked.get()).pack()
+
 clicked = StringVar()
-clicked.set('10 Mbps')
-drop = OptionMenu(root, clicked,'10 Mbps','20 Mbps', '30 Mbps','40 Mbps','50 Mbps')
-drop.pack(anchor='nw', pady=250, padx=250, ipadx=98)
+clicked.set('male')
+
+drop = OptionMenu(root, clicked,'male','female', 'other')
+drop.pack(anchor='nw',pady=315,padx=188,ipadx=108)
+
 # myButton = Button(root, text='gender', command=show) .pack()
 #myButton.grid(row=4, column=1, padx=10, pady=10, sticky=W)
-lbl_contact=Label(my_frame, text='CONTACT NO.:', font=('Arial Black',16,'bold'),fg='WHITE',bg='BLACK')
-lbl_contact.grid(row=5, column=0, padx=20, pady=10,ipady=20, sticky=W)
-contact_entry=Entry(my_frame,font=('Arial Black',16,'bold'))
-contact_entry.grid(row=5, column=1, padx=20, pady=10, sticky=W)
-lbl_address=Label(my_frame, text='ADDRESS:', font=('Arial Black',16,'bold'),fg='WHITE',bg='BLACK')
-lbl_address.grid(row=6, column=0, padx=20, pady=10, sticky=W)
-add_entry=Entry(my_frame,font=('Arial Black',16,'bold'))
-add_entry.grid(row=6, column=1, padx=20, pady=10, sticky=W)
-lbl_username=Label(my_frame, text='USERNAME:', font=('Arial Black',16,'bold'),fg='WHITE',bg='BLACK')
-lbl_username.grid(row=6, column=0, padx=20, pady=0, sticky=W)
-username_entry=Entry(my_frame,font=('Arial Black',16,'bold'))
-username_entry.grid(row=6, column=1, padx=20, pady=10, sticky=W)
-lbl_email=Label(my_frame, text='EMAIL:', font=('Arial Black',16,'bold'),fg='WHITE',bg='BLACK')
-lbl_email.grid(row=7, column=0, padx=20, pady=10, sticky=W)
-email_entry=Entry(my_frame,font=('Arial Black',16,'bold'))
-email_entry.grid(row=7, column=1, padx=20, pady=10, sticky=W)
-lbl_duration=Label(my_frame, text='DURATION:', font=('Arial Black',16,'bold'),fg='white',bg='black')
-lbl_duration.grid(row=8, column=0, padx=20, pady=45, sticky=W)
-def show():
-    myLabel= Label(root, text=clicked.get()) #.pack()
-clicked = StringVar()
-clicked.set('MONTHLY')
-drop = OptionMenu(root, clicked, 'YEARLY','HALF-YEARLY', 'QUATERLY','MONTHLY','WEEKLY')
-# drop.grid(row=5, column=1, padx= 20, pady=10, ipadx=20, sticky=W)
-drop.pack(anchor='nw',pady=10,padx=238,ipadx=98, ipady=5)
-# Button Frame
-btn_frame=Frame(root,bd = 4, relief=RIDGE, bg='BLACK')
-btn_frame.place(x=40, y=650, width=500)
-add_btn=Button(btn_frame, text='ADD', width=13, height=2, fg='BLACK', bg='WHITE')
-add_btn.grid(row=9, column=0, padx=10, pady=10)
-update_btn=Button(btn_frame, text='UPDATE', width=13, height=2, fg='BLACK', bg='WHITE')
-update_btn.grid(row=9, column=1, padx=10, pady=10)
-delete_btn=Button(btn_frame, text='DELETE', width=13, height=2, fg='BLACK', bg='WHITE')
-delete_btn.grid(row=9, column=2, padx=10, pady=10)
-clear_btn=Button(btn_frame, text='CLEAR', width=13, height=2, fg='BLACK', bg='WHITE')
-clear_btn.grid(row=9, column=3, padx=10, pady=10)
-#detail frame
-Details_frame = Frame(root, bd=4, relief = RIDGE, bg= 'BLACK')
-Details_frame.place(x=620, y= 80, width=900, height=650)
-search_lbl=Label(Details_frame, text='SEARCH BY:', font=('Arial Black',16,'bold'),fg='WHITE',bg='BLACK')
-search_lbl.grid(row=1, column=0, padx=20, pady=10, sticky=W)
-def show():
-    myLabel= Label(Details_frame, text=clicked.get()).pack()
-clicked = StringVar()
-clicked.set('')
-drop = OptionMenu(Details_frame, clicked,'CUSTOMER ID', 'NAME','USERNAME','CONTACT NO.','USERNAME')
-drop.grid(row=1, column=1, padx= 15, pady=10, ipadx=5, sticky=W)
-search_entry=Entry(Details_frame,font=('Arial Black',16,'bold'))
-search_entry.grid(row=1, column=2, padx=3, pady=10,ipadx=5, sticky=W)
-search_btn=Button(Details_frame, text='SEARCH', width=13, height=2, fg='BLACK', bg='WHITE')
-search_btn.grid(row=1, column=3, padx=10, pady=10)
-show11_btn=Button(Details_frame, text='SHOW ALL', width=13, height=2, fg='BLACK', bg='WHITE')
-show11_btn.grid(row=1, column=4, padx=10, pady=10)
-#table frame
-table_frame = Frame(Details_frame, bd=4, relief = RIDGE, bg= 'WHITE')
-table_frame.place(x=10, y= 70, width=865, height=550)
-SVBar = Scrollbar(table_frame)
-SVBar.pack(side=RIGHT, fill="y")
-SHBar = Scrollbar(table_frame, orient=HORIZONTAL)
-SHBar.pack(side=BOTTOM, fill="x")
 
-root.mainloop()
+lbl_contact=Label(my_frame, text='CONTACT', font=('times new roman',20,'bold'),fg='white',bg='black')
+lbl_contact.grid(row=5, column=0, padx=20, pady=10, sticky=W)
+
+contact_entry=Entry(my_frame,font=('times new roman',20,'bold'))
+contact_entry.grid(row=5, column=1, padx=20, pady=10, sticky=W)
+
+lbl_package=Label(my_frame, text='PACKAGE', font=('times new roman',20,'bold'),fg='white',bg='black')
+lbl_package.grid(row=6, column=0, padx=20, pady=10, sticky=W)
+
+package_entry=Entry(my_frame,font=('times new roman',20,'bold'))
+package_entry.grid(row=6, column=1, padx=20, pady=10, sticky=W)
+
+lbl_address=Label(my_frame, text='ADDRESS', font=('times new roman',20,'bold'),fg='white',bg='black')
+lbl_address.grid(row=7, column=0, padx=20, pady=10, sticky=W)
+
+add_entry=Entry(my_frame,font=('times new roman',20,'bold'))
+add_entry.grid(row=7, column=1, padx=20, pady=10, sticky=W)
+
+
+def submit():
+    # Create a database or connect to one
+    conn = sqlite3.connect('database.db')
+
+    c = conn.cursor()
+
+    # Insert into table
+    c.execute("INSERT INTO sub_data VALUES (:userid_entry, :name_entry, :email_entry, :contact_entry, :package_entry, :add_entry)",{
+        'userid_entry' : userid_entry.get(),
+        'name_entry': name_entry.get(),
+        'email_entry': email_entry.get(),
+        'contact_entry': contact_entry.get(),
+        'package_entry': package_entry.get(),
+        'add_entry': add_entry.get()
+    })
+
+    messagebox.showinfo("User Data", "YOUR DATA IS REGISTERED")
+
+    conn.commit()
+
+    conn.close()
+
+    userid_entry.delete(0, END)
+    name_entry.delete(0, END)
+    email_entry.delete(0, END)
+    contact_entry.delete(0, END)
+    package_entry.delete(0, END)
+    add_entry.delete(0, END)
+
+
+def delete():
+    con1 = sqlite3.connect("database.db")
+
+    cur1 = con1.cursor()
+
+    cur1.execute("DELETE from sub_data WHERE subscriber_user_id = " + userid_entry.get())
+
+    messagebox.showinfo("Yes", "Deleted Successfully")
+
+    con1.commit()
+    con1.close()
+
+def update():
+    # Creating a database or connect to one
+    con1 = sqlite3.connect('database.db')
+
+    # Create cursor
+    cur1 = con1.cursor()
+
+    record_id = userid_entry.get()
+
+    cur1.execute(""" UPDATE sub_data SET
+            subscriber_user_id = :sub_user_id,
+            subscriber_name = :sub_name,
+            subscriber_email = :sub_email,
+            subscriber_contact = :sub_contact,
+            subscriber_package = :sub_dob,
+            subscriber_address = :sub_address
+            WHERE subscriber_user_id = :subscriber_user_id""",
+            {'sub_roll_no': subscriber_userid_editor.get(),
+            'sub_name': subscriber_name_editor.get(),
+            'sub_email': subscriber_email_editor.get(),
+            'sub_contact': subscriber_contact_editor.get(),
+            'sub_package': subscriber_package_editor.get(),
+            'sub_address': subscriber_address_editor.get(),
+            'subscriber_user_id': record_id
+            }
+    )
+    con1.commit()
+    con1.close()
+    # Updating all the data and closing window
+    messagebox.showinfo("UPDATE","Your data has been updated!")
+    editor.destroy()
+
+def edit():
+    global editor
+
+    editor = Tk()
+    editor.title('Update Data')
+    editor.geometry('500x500')
+    editor.config(bg="silver")
+
+    # Create a database or connect to one
+    con1 = sqlite3.connect('database.db')
+
+    # Create cursor
+    cur1 = con1.cursor()
+
+    record_id = roll_entry.get()
+
+    # Query of the database
+    cur1.execute("SELECT * FROM sub_data WHERE subscriber_user_id=" + record_id)
+
+    records = cur1.fetchall()
+
+    # Creating global variable for all text boxes
+    global subscriber_userid_editor
+    global subscriber_name_editor
+    global subscriber_email_editor
+    global subscriber_contact_editor
+    global subscriber_package_editor
+    global subscriber_address_editor
+
+    # Creating texy boxes
+    subscriber_userid = Label(editor, font=("Times New Roman", 15, "bold"), width=18, text="Subscriber User Id ")
+    subscriber_userid.place(x=10, y=50)
+
+    subscriber_userid_editor = Entry(editor, font=("Times New Roman", 15, "bold"), width=18)
+    subscriber_userid_editor.place(x=260, y=50)
+
+    subscriber_name = Label(editor, font=("Times New Roman", 15, "bold"), width=18, text="Subscriber Name")
+    subscriber_name.place(x=10, y=100)
+
+    subscriber_name_editor = Entry(editor, font=("Times New Roman", 15, "bold"), width=18)
+    subscriber_name_editor.place(x=260, y=100)
+
+    subscriber_email = Label(editor, font=("Times New Roman", 15, "bold"), text="Subscriber Email", width=18)
+    subscriber_email.place(x=10, y=150)
+
+    subscriber_email_editor = Entry(editor, font=("Times New Roman", 15, "bold"), width=18)
+    subscriber_email_editor.place(x=260, y=150)
+
+    subscriber_contact = Label(editor, font=("Times New Roman", 15, "bold"), text="Subscriber Contact", width=18)
+    subscriber_contact.place(x=10, y=200)
+
+    subscriber_contact_editor = Entry(editor, font=("Times New Roman", 15, "bold"), width=18)
+    subscriber_contact_editor.place(x=260, y=200)
+
+    subscriber_dob = Label(editor, font=("Times New Roman", 15, "bold"), width=18, text="Subscriber DOB")
+    subscriber_dob.place(x=10, y=250)
+
+    subscriber_package_editor = Entry(editor, font=("Times New Roman", 15, "bold"), width=18)
+    subscriber_package_editor.place(x=260, y=250)
+
+    subscriber_address = Label(editor, font=("Times New Roman", 15, "bold"), width=18, text="Subscriber Address")
+    subscriber_address.place(x=10, y=300)
+
+    subscriber_address_editor = Entry(editor, font=("Times New Roman", 15, "bold"), width=18)
+    subscriber_address_editor.place(x=260, y=300)
+
+    save_btn = Button(editor, font=("Times New Roman", 15, "bold"), text="Save", command=update)
+    save_btn.place(x=200, y=400)
+
+    # Loop through the results
+    for record in records:
+        subscriber_userid_editor.insert(0, record[0])
+        subscriber_name_editor.insert(0, record[1])
+        subscriber_email_editor.insert(0, record[2])
+        subscriber_contact_editor.insert(0, record[3])
+        subscriber_package_editor.insert(0, record[4])
+        subscriber_address_editor.insert(0, record[5])
+
+
+# Button Frame
+btn_frame=Frame(root,bd = 4, relief=RIDGE, bg='grey')
+btn_frame.place(x=30, y=600, width=500)
+
+add_btn=Button(btn_frame, text='ADD', width=13, height=2,font=("Times New Roman", 10, "bold"), fg='black', bg='yellow', command=submit)
+add_btn.grid(row=0, column=0, padx=10, pady=10, ipadx=20)
+
+update_btn=Button(btn_frame, text='UPDATE', width=13, height=2, font=("Times New Roman", 10, "bold"), fg='black', bg='yellow', command=edit)
+update_btn.grid(row=0, column=1, padx=10, pady=10, ipadx=20)
+
+delete_btn=Button(btn_frame, text='DELETE', width=13, height=2, font=("Times New Roman", 10, "bold"), fg='black', bg='yellow', command=delete)
+delete_btn.grid(row=0, column=2, padx=10, pady=10, ipadx=20)
+
+
+#detail frame
+Details_frame = Frame(root, bd=4, relief = RIDGE, bg= 'grey')
+Details_frame.place(x=600, y= 80, width=900, height=600)
+
+search_lbl=Label(Details_frame, text='INFORMATIONS OF SUBSCRIBER', font=('times new roman',20,'bold'),fg='black',bg='grey')
+search_lbl.grid(row=1, column=0, padx=20, pady=10, sticky=W)
+
+# Function for viewing data
+def View():
+    con1 = sqlite3.connect("database.db")
+    cur1 = con1.cursor()
+    cur1.execute("SELECT * FROM sub_data")
+    rows = cur1.fetchall()
+    for row in rows:
+        Subscriber_table.insert("", tk.END, values=row)
+    con1.close()
+
+
+show11_btn=Button(Details_frame, text='SHOW ALL', width=13, height=2,font=("Times New Roman", 10, "bold"), fg='white', bg='black', command=View)
+show11_btn.grid(row=1, column=4, padx=10, pady=10, ipadx=20)
+
+# Table frame
+Table_frame= Frame(Details_frame , bd=4, relief=RIDGE, bg='grey')
+Table_frame.place(x=15, y=60, width=865, height=490)
+
+
+scroll_x = Scrollbar(Table_frame, orient=HORIZONTAL)
+scroll_y = Scrollbar(Table_frame, orient=VERTICAL)
+
+Subscriber_table= ttk.Treeview(Table_frame,columns=("userid", "name", "email", "contact", "package", "address"),
+                                    xscrollcommand=scroll_x.set, yscrollcommand=scroll_y.set)
+scroll_x.pack(side=BOTTOM, fill=X)
+scroll_y.pack(side=RIGHT, fill=Y)
+scroll_x.config(command=Subscriber_table.xview)
+scroll_y.config(command=Subscriber_table.yview)
+
+
+Subscriber_table.heading("userid", text="Roll No.")
+Subscriber_table.column("userid", width=100)
+Subscriber_table.heading("name", text="Name")
+Subscriber_table.column("name", width=100)
+Subscriber_table.heading("email", text="Email")
+Subscriber_table.column("email", width=100)
+
+Subscriber_table.heading("contact", text="Contact")
+Subscriber_table.column("contact", width=100)
+Subscriber_table.heading("package", text="DOB")
+Subscriber_table.column("package", width=100)
+Subscriber_table.heading("address", text="Address")
+Subscriber_table.column("address", width=100)
+Subscriber_table['show'] = 'headings'
+Subscriber_table.pack(fill=BOTH, expand=1)
+
+def endProgam():
+    raise SystemExit
+    sys.exit()
+
+
+exit_btn=Button(root, text='EXIT', width=13, height=2, font=("Times New Roman", 10, "bold"), fg='white', bg='black', command = endProgam)
+exit_btn.place(x=1393, y=635 )
+
+mainloop()
